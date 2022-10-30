ee14886efda4818d509f74b655a5c169e6e1610d
Rani <109195584+ranisharma09@users.noreply.github.com>
2022-10-30 12:50:19 +0530

    Create Robot.py

---
+++ Robot.py
@@ -0,0 +1,69 @@
+#coding by raNi TRIXCKER 
+#fb Id'z https://www.facebook.com/mistyxd0.2
+
+# https://youtube.com/channel/UCbT--Z1XzQpSUgjD6bfzzUA
+
+#coding=utf
+
+
+import turtle
+t= turtle.Turtle()
+s=turtle.Screen()
+s.bgcolor("skyblue")
+t.pencolor("red")
+t.fillcolor("red")
+t.pensize(7)
+t.begin_fill()
+for i in range(0,4):
+  t.forward(50)
+  t.left(90)
+t.end_fill()
+
+t.pencolor("white")
+t.penup()
+t.left(90)
+t.forward(30)
+t.right(90)
+t.forward(12)
+t.pendown()
+t.pensize(3)
+t.circle(5)
+t.penup()
+t.forward(25)
+t.pendown()
+t.circle(5)
+
+
+t.penup()
+t.right(90)
+t.forward(20)
+t.right(90)
+t.pendown()
+
+t.fillcolor("white")
+t.begin_fill()
+t.forward(23)
+t.right(90)
+t.forward(7)
+t.right(90)
+t.forward(23)
+t.right(90)
+t.forward(7)
+t.right(90)
+t.end_fill()
+
+t.penup()
+t.forward(25)
+t.right(90)
+t.forward(35)
+t.pendown()
+
+t.pencolor("red")
+t.forward(30)
+t.penup()
+t.right(90)
+t.pendown()
+t.fillcolor("red")
+t.begin_fill()
+t.circle(5)
+t.end_fill()
